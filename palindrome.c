2685f12bdf71e22f8056b795f19b34944a14a94a
ShivamJ17 <shivamjain17021996@gmail.com>
2019-12-06 06:24:38 -0800

    palindrome program commited

---
+++ palindrome.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() {
+    int n, rev = 0, rem, num;
+    printf("Enter a number");
+    scanf("%d", &n);
+    num=n;
+    while(n!=0) 
+   {
+        rem=n%10;
+        rev=(rev*10)+rem;
+        n=n/10;
+    }
+
+    if (num==rev)
+        printf("%d is a palindrome.",num);
+    else
+        printf("%d is not a palindrome.",num);
+   }
